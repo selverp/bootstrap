953723bb3358404123260321dc60e70d35fe38bb
selverp <selverp@gmail.com>
2022-01-09 18:08:50 +0100

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include "header.php"; ?>
-   <!-- <h1>Responsive Website</h1> -->
+   <!-- <h1>Responsive Website 2</h1> -->
    <header class="container-fluid">
       <section id="top" class="bg-primary">
         <div class="top container">
